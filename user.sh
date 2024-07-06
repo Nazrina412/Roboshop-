ad71f2f76b927a51e719984abb7b4a858abf8bcf
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 08:31:36 -0400

    user scripting

--- user.sh
+++ user.sh
@@ -1,13 +1,13 @@
-cp user.service /systemd/system/user.service
+cp user.service /etc/systemd/system/user.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
+cd/app
 unzip /tmp/user.zip
-cd /app
+cd/app
 npm install
 systemctl daemon-reload
 systemctl enable user
