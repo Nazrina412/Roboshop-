effd9e0ba705c3e8d64ba0fd9d397070d1672e34
Nazrina <nazrinakhan88@gmail.com>
2024-07-05 22:45:05 -0400

    catalogue Scripting

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,7 +1,7 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-cp catalogue.service /etc/systemd/system/catalogue.service
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
