e5b7c86c0d0c663b70fb1af540089d7efb858158
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 10:17:58 -0400

    nginx scripting

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
-cp nginx.conf/etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl start nginx
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
