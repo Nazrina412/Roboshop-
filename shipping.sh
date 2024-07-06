1ff89dada136124bf8c158770eed7905acceb271
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 10:46:51 -0400

    user scripting

--- shipping.sh
+++ shipping.sh
@@ -3,9 +3,9 @@ dnf install maven -y
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
-cd/app
+cd /app
 unzip /tmp/shipping.zip
-cd/app
+cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 systemctl daemon-reload
--- user.sh
+++ user.sh
@@ -6,9 +6,9 @@ useradd roboshop
 rm -rf /app
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd/app
+cd /app
 unzip /tmp/user.zip
-cd/app
+cd /app
 npm install
 systemctl daemon-reload
 systemctl enable user
