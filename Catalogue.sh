d902af166d052518890a29821ac5808d01ca9449
Nazrina <nazrinakhan88@gmail.com>
2024-07-05 22:54:03 -0400

    catalogue Scripting

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,4 +1,5 @@
 cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
@@ -7,12 +8,13 @@ mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 dnf install mongodb-mongosh -y
 mongosh --host mongodb.dev.naifah.online </app/db/master-data.js
 
