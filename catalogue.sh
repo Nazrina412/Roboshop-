bb8ce29277884139f56eb2535b1e5d4241d01dbb
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 20:58:27 -0400

    learn variables

--- catalogue.sh
+++ catalogue.sh
@@ -3,9 +3,9 @@ component=catalogue
 NODEJS
 
 echo installing mongodb
-dnf install mongodb-mongosh -y &>LOG_FILE
+dnf install mongodb-mongosh -y &>>LOG_FILE
 
 echo connecting to master data
-mongosh --host mongodb.dev.naifah.online </app/db/master-data.js &>LOG_FILE
+mongosh --host mongodb.dev.naifah.online </app/db/master-data.js &>>LOG_FILE
 
 
--- common.sh
+++ common.sh
@@ -2,35 +2,35 @@ LOG_FILE=/tmp/roboshop.log
 
 NODEJS() {
   echo disable Nodejs Default vesion
-  dnf module disable nodejs -y &>LOG_FILE
+  dnf module disable nodejs -y &>>LOG_FILE
 
   echo enable Nodejs 20 module
-  dnf module enable nodejs:20 -y &>LOG_FILE
+  dnf module enable nodejs:20 -y &>>LOG_FILE
   echo install Nodejs
-  dnf install nodejs -y >$LOG_FILE
+  dnf install nodejs -y &>>LOG_FILE
 echo copy service file
-cp ${component}.service /etc/systemd/system/${component}.service &>LOG_FILE
+cp ${component}.service /etc/systemd/system/${component}.service &>>LOG_FILE
 
 echo copy mongorepo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>LOG_FILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
 
 echo adding application user
-useradd roboshop &>LOG_FILE
+useradd roboshop &>>LOG_FILE
 
 echo cleaning old content
-rm -rf /app &>LOG_FILE
+rm -rf /app &>>LOG_FILE
 
 echo creating app directory
-mkdir /app &>LOG_FILE
+mkdir /app &>>LOG_FILE
 
 echo download app content
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>LOG_FILE
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>LOG_FILE
 
 
 cd /app
 
 echo extract Nodejs Dependencies
-unzip /tmp/${component}.zip &>LOG_FILE
+unzip /tmp/${component}.zip &>>LOG_FILE
 
 
 cd /app
