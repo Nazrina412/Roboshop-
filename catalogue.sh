a296814cf273dcdb62015026c66b9a88349cd1a9
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 21:34:16 -0400

    learn variables

--- catalogue.sh
+++ catalogue.sh
@@ -4,9 +4,17 @@ NODEJS
 
 PRINT installing mongodb
 dnf install mongodb-mongosh -y &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT connecting to master data
 mongosh --host mongodb.dev.naifah.online </app/db/master-data.js &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
--- common.sh
+++ common.sh
@@ -11,57 +11,105 @@ PRINT() {
 NODEJS() {
  PRINT disable Nodejs Default vesion
   dnf module disable nodejs -y &>>LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
   PRINT enable Nodejs 20 module
   dnf module enable nodejs:20 -y &>>LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+      echo success
+      else
+      echo failure
+      fi
 
   PRINT install Nodejs
   dnf install nodejs -y &>>LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+      echo success
+      else
+      echo failure
+      fi
 
 PRINT copy service file
 cp ${component}.service /etc/systemd/system/${component}.service &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT copy mongorepo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT adding application user
 useradd roboshop &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT cleaning old content
 rm -rf /app &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT creating app directory
 mkdir /app &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT download app content
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 
 cd /app
 
 PRINT extract Nodejs Dependencies
 unzip /tmp/${component}.zip &>>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 cd /app
 
 PRINT downlad Nodejs dependencies
 npm install &>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 PRINT start service
 systemctl daemon-reload >/tmp/roboshop.log
 systemctl enable ${component} &>LOG_FILE
 systemctl restart ${component} &>LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+    echo success
+    else
+    echo failure
+    fi
 
 }
