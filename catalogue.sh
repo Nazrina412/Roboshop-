2e3d47832a5904f775dc7f0d2428e85d726a0f1b
Nazrina <nazrinakhan88@gmail.com>
2024-07-07 19:38:40 -0400

    learn variables

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,6 @@
 source common.sh
 component=catalogue
 app_path=/app
+schema_setup=mongo
+
 NODEJS
-
-PRINT installing mongodb
-dnf install mongodb-mongosh -y &>>LOG_FILE
-STAT $?
-
-PRINT connecting to master data
-mongosh --host mongodb.dev.naifah.online </app/db/master-data.js &>>LOG_FILE
-STAT $?
-
