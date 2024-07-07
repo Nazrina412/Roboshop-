f1405e725d528c7f6e1828f686578081aa1e38b0
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 21:48:46 -0400

    learn variables

--- catalogue.sh
+++ catalogue.sh
@@ -4,17 +4,9 @@ NODEJS
 
 PRINT installing mongodb
 dnf install mongodb-mongosh -y &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT connecting to master data
 mongosh --host mongodb.dev.naifah.online </app/db/master-data.js &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
--- common.sh
+++ common.sh
@@ -4,112 +4,74 @@ rm -f $LOG_FILE
 PRINT() {
   echo
   echo
-  echo "########################## $* #################################"
+  echo "########################## $* #################################"  &>>LOG_FILE
+  echo $*
 
-}
+  }
+  STAT() {
+    if [ $1 -eq o]; then
+      echo success
+      else
+        echo failure
+        fi
+  }
+
 
 NODEJS() {
  PRINT disable Nodejs Default vesion
   dnf module disable nodejs -y &>>LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
+
 
   PRINT enable Nodejs 20 module
   dnf module enable nodejs:20 -y &>>LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo success
-      else
-      echo failure
-      fi
+ STAT $?
 
   PRINT install Nodejs
   dnf install nodejs -y &>>LOG_FILE
-  if [ $? -eq 0 ]; then
-      echo success
-      else
-      echo failure
-      fi
+  STAT $?
 
 PRINT copy service file
 cp ${component}.service /etc/systemd/system/${component}.service &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT copy mongorepo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT adding application user
 useradd roboshop &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT cleaning old content
 rm -rf /app &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT creating app directory
 mkdir /app &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT download app content
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 
 cd /app
 
 PRINT extract Nodejs Dependencies
 unzip /tmp/${component}.zip &>>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 cd /app
 
 PRINT downlad Nodejs dependencies
 npm install &>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 PRINT start service
 systemctl daemon-reload >/tmp/roboshop.log
 systemctl enable ${component} &>LOG_FILE
 systemctl restart ${component} &>LOG_FILE
-if [ $? -eq 0 ]; then
-    echo success
-    else
-    echo failure
-    fi
+STAT $?
 
 }
